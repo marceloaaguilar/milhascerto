f36052142be02372aee4173c7584d2aa08291ec1
Marcelo Aguilar <marcelodalmeida42@gmail.com>
2023-11-12 12:54:03 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,47 @@
+
+  if((window.location.href.includes("destravandocodigomilhas")) && screen.width < 767){
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+  }
+
+  else if(window.location.href.includes("destravandocodigomilhas")){
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+  }
+
+
+  $(".itemContainer").mouseover(function(){
+        $(this).children("p").css("display", "block");
+
+
+  })
+
+  $(".item").mouseout(function(){
+    $(".descriptionCarousel").css("display","none");
+})
+
+
+
+   
+
+
+    if (window.location.href.includes("sorteios")){
+        var swiper = new Swiper(".swiper2", {
+            pagination: {
+              el: ".swiper-pagination",
+            },
+          });
+
+    }
